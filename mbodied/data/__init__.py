0dafcc37f4a083bd3f22fbcaf316d2f2d0345ac2
Sebastian Peralta <peraltas@seas.upenn.edu>
2024-09-08 03:43:46 -0400

    feat: provide backwards compatibility for old imports

--- mbodied/data/__init__.py
+++ mbodied/data/__init__.py
@@ -1,26 +1,27 @@
+# Provide backwards compatibility for old imports
 import sys
+import warnings
 from typing import Any, Callable
 
-import embdata
 from embdata import episode, features, sense
 from embdata.sample import Sample
 from embdata.utils.import_utils import smart_import
+
+# Re-export the modules and functions
+
+
+# Import and re-export the to_features function
+from embdata.features import to_features_dict
 
 
 def getattr_migration(module_name: str) -> Callable[[str], Any]:
     """Implement PEP 562 for objects that were either moved or removed on the migration to V2."""
     def wrapper(name: str) -> Any:
-        """Raise an error if the object is not found, or warn if it was moved."""
-        import warnings
-
         if name == "__path__":
             raise AttributeError(f"module {module_name!r} has no attribute {name!r}")
         try:
-            if name not in sys.modules:
-                import_path = f"embdata.{name}"
-                imported_module = smart_import(import_path)
-            else:
-                imported_module = sys.modules[name]
+            import_path = f"embdata.{name}"
+            imported_module = smart_import(import_path)
         except ModuleNotFoundError as e:
             raise AttributeError(f"module {module_name!r} has no attribute {name!r}") from e
         else:
@@ -29,6 +30,6 @@ def getattr_migration(module_name: str) -> Callable[[str], Any]:
 
     return wrapper
 
-
 __getattr__ = getattr_migration(__name__)
-__all__ = ['Sample', 'sense', 'episode', 'features']
+
+__all__ = [getattr(__name__, name) for name in dir(sys.modules[__name__]) if not name.startswith("_")]
