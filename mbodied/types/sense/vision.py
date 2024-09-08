03ecaaef7eb10a5e27db86a143657497e2edb11a
Sebastian Peralta <peraltas@seas.upenn.edu>
2024-09-08 04:31:51 -0400

    fix: Remove unused import statement in vision.py

--- mbodied/types/sense/vision.py
+++ mbodied/types/sense/vision.py
@@ -266,7 +266,6 @@ class Image(Sample):
 
         try:
             # Open the URL and read the image data
-            import urllib.request
 
             user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
             headers = {
